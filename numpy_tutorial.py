4b76586d1097ceba82d2e183b4fabebd28c224fc
Magambo Gatete Elie <eliemagambo@gmail.com>
2017-04-03 20:30:19 +0900

    Added slicing/indexing numpy arrays

--- numpy_tutorial.py
+++ numpy_tutorial.py
@@ -1,5 +1,6 @@
 #Numpy exploration with Python 2.7
 from math import sqrt
+import numpy as np
 
 #1. Numbers
 title="Number title"
@@ -151,5 +152,114 @@ class Greeter(object):
 g=Greeter("Fred")
 print g.greet(True)
 print g.greet()
+
+
+#9 Numpy datatypes
+
+title="Working with Numpy datatypes"
+print "Rank one array has a structure of a list"
+a=np.array([1,2,3])
+print a
+print "Shape"
+print a.shape
+print "First element is the number of elements counting from the outmost []"
+b=np.array([[2,3,4],[5,6,8]])
+print b
+print "Shape"
+print b.shape
+print "array is indexed from zero,b[0,2] is 4,b[1,0] is 5"
+print b[0,2]
+print b[1,0]
+
+#10 initializing numpy arrays
+title="Initializing numpy array"
+print "Initializing with zeros(2,2)"
+a=np.zeros((2,2))
+print a
+
+print "initializing with ones(4,4)"
+print np.ones((4,4))
+
+print "initialzing with a default value with full((2,2),3)"
+print np.full((2,2),3)
+
+print "Creating identity matrix of dimension 3x3"
+print np.eye(3)
+
+print "Initializing with random values of an array of 3 elements with 4 random values each"
+
+print np.random.random((3,4))
+#11 index array
+title="Indexing array and slicing them"
+print title
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print a
+b=a[:2,1:3]
+print "sliced b=a[:2,1:3]"
+print b
+
+print a[0,1]
+print "Change the data of a slice of an array will eventually lead to the change of the original array b[0,0]=77 will change a[0,1]"
+b[0,0]=77
+print "b[0,0]"
+print b[0,0]
+print "a[0,1]"
+print a[0,1]
+print "Changed a"
+print a
+print "the second second row alone of a print row_1=a[1,:] which will be array of shape(4,) with rank 1"
+row_1=a[1,:]
+print row_1,row_1.shape
+print "Print selection from row 1 until 2 but doesn't include 2 row,row_2=[1:2,:] which keeps the shape of (1 row,3 columns)"
+row_2=a[1:2,:]
+print row_2,row_2.shape
+print "one column of shape (,3) but since is rank one it becomes same as row(3,)"
+col_1=a[:,1]
+print col_1,col_1.shape
+print "columns from 1 to 2 indexes wihout including 2 of all rows with shape(3,1)"
+col_2=a[:,1:2]
+print col_2,col_2.shape
+
+#12 indexing using integer
+title="Indexing using integers"
+c=np.array([[1,2],[3,4],[5,6]])
+print "Indexing with integer,usually the first index list describe the first row coordinates of an element and the second describe the columns cooridnate of element to be selected c[[0,1,2],[0,1,0]]"
+print c[[0,1,2],[0,1,0]]
+print np.array([c[0,0],c[1,1],c[2,0]])
+print "Indexing with integer one array of column indexes desired to change the rows"
+d=np.array([1,0])
+c[np.arange(2),d]+=10
+print c
+
+#13 indexing using boolean
+
+title="Indexing using boolean values"
+print title
+bool_idx=(c>2)
+print bool_idx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
               
           
